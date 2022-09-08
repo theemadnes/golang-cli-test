f1ea0dcd7e6890374d2091acbcd481fbe646f0e1
theemadnes <alex.mattson@gmail.com>
2022-09-07 21:35:31 -0500

    basic file import working

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Revisions struct {
+	Revision []struct {
+		RevisionId string `yaml:"revision_id"`
+		Region     string `yaml:"region"`
+		ProjectId  string `yaml:"project_id"`
+	} `yaml:"revisions"`
+}
+
+func readRevisions(filename string) (Revisions, error) {
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//r := &revisions{}
+	r := Revisions{}
+	err = yaml.Unmarshal(buf, &r)
+	if err != nil {
+		return r, fmt.Errorf("in file %q: %v", filename, err)
+	}
+
+	return r, nil
+}
+
+func main() {
+
+	// get file name from command line
+	argsWithoutProg := os.Args[1:]
+
+	fmt.Printf("Reading manifest file %v\n", string(argsWithoutProg[0]))
+
+	r, err := readRevisions(argsWithoutProg[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%v", r.Revision[1].RevisionId)
+
+}
